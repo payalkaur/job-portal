f43241549b5048c705a97b47cfc90c3f155e12fd
pawandeepkaur <payalkaur1706@gmail.com>
2026-03-25 15:27:58 +0530

    Implemented user authentication

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -20,13 +20,13 @@ const LandingPage = () => {
   return (
     <main className="flex flex-col gap-10 sm:gap-20 py-10 sm:py-20">
       <section className="text-center ">
-        <h1 className="flex flex-col items-center justify-center gradient-title font-extrabold text-4xl sm:text-6xl lg:text-8xl tracking-tighter py-4">
+        <h1 className="flex flex-col items-center justify-center gradient-title font-extrabold text-3xl sm:text-5xl lg:text-7xl tracking-tighter py-4">
           Find Your Dream Job
-          <span className="flex items-center gap-2 sm:gap-6">
+          <span className="flex items-center gap-1 sm:gap-5">
             and get
             <img
               src="/logo.png"
-              className="h-14 sm:h-24 lg:h-32"
+              className="h-12 sm:h-20 lg:h-28"
               alt="Hirrd Logo"
             />
           </span>
